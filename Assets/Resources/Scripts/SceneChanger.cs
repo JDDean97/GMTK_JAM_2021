3528750bb6ec404755e434d53b389a6687874b96
Progtart <ramenhero77@hotmail.com>
2021-06-11 19:56:41 -0700

    Updated main menu and added loading screen

---
+++ Assets/Resources/Scripts/SceneChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger: MonoBehaviour
+{
+    public void load(int sceneNumber)
+    {
+        GameObject loader = Resources.Load<GameObject>("Prefabs/LoadScreen");
+        GameObject.Instantiate(loader);
+        SceneManager.LoadSceneAsync(sceneNumber);
+    }
+}
