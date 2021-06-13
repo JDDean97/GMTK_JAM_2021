c40ce0ef2118392bbea2a3dfa748712aef1ae6c9
Adam Paquette <acp263@nau.edu>
2021-06-13 03:26:41 -0700

    Switched to line renderer

--- Assets/Resources/Scripts/RopeRenderer.cs
+++ Assets/Resources/Scripts/RopeRenderer.cs
@@ -9,14 +9,11 @@ public class RopeRenderer : MonoBehaviour
     private GameObject climber2;
     private GameObject ropeStart;
     private GameObject ropeEnd;
-    private GameObject currentSegment;
-    private Vector3 ropeStartAnchor;
-    private Vector3 ropePosition;
-    private Vector3 localRopePosition;
-    public int numberOfSegments = 30;
-    public float scaleFactor = 0.03f;
-    public float mass = 2;
-    public float totalRopeLength;
+    public int numberOfSegments = 20;
+    private int totalRopeLength;
+    private float ropeSegLen = 1.0f;
+    private float lineWidth = 0.5f;
+    private LineRenderer lineRenderer;
     private List<RopeSegment> ropeSegments = new List<RopeSegment>();
 
     // Start is called before the first frame update
@@ -28,52 +25,66 @@ public class RopeRenderer : MonoBehaviour
         ropeStart = climber1.transform.Find("rope_attachment").gameObject;
         ropeEnd = climber2.transform.Find("rope_attachment").gameObject;
 
-        ropePosition = ropeStart.transform.position;
-        localRopePosition = ropeStart.transform.localPosition;
+        totalRopeLength = (int)FindObjectOfType<Director>().ropeLength;
 
-        currentSegment = climber1;
+        this.lineRenderer = this.GetComponent<LineRenderer>();
+        Vector3 ropeStartPoint = ropeStart.transform.position;
 
-        AttachInitalRopeSegment(currentSegment);
-        ropeSegments.Add(new RopeSegment(currentSegment));
-        for (int i = 2; i <= numberOfSegments; i++)
+        for (int i = 0; i < totalRopeLength; i++)
         {
-          AttachRopeSegment(currentSegment, i);
-          ropeSegments.Add(new RopeSegment(currentSegment));
+            this.ropeSegments.Add(new RopeSegment(ropeStartPoint));
+            ropeStartPoint.y -= ropeSegLen;
         }
-        AttachFinalRopeSegment(currentSegment);
-        ropeSegments.Add(new RopeSegment(currentSegment));
-
-        totalRopeLength = ropeSegments[0].ropeLength * numberOfSegments;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        totalRopeLength = (int)FindObjectOfType<Director>().ropeLength;
+
+        if (numberOfSegments > totalRopeLength) {
+          ropeSegments.RemoveAt(numberOfSegments - 1);
+          numberOfSegments -= 1;
+        }
+        else if(numberOfSegments < totalRopeLength) {
+          ropeSegments.Add(new RopeSegment(ropeEnd.transform.position));
+          numberOfSegments += 1;
+        }
+
         Vector3 gravity = new Vector3(0.0f, -0.5f, 0.0f);
 
         for(int i = 0; i < numberOfSegments; i++)
         {
-            RopeSegment currentSegment = ropeSegments[i];
-            Vector3 velocity = currentSegment.nowPos - currentSegment.oldPos;
-            currentSegment.oldPos = currentSegment.nowPos;
-            currentSegment.nowPos += velocity;
-            currentSegment.nowPos += gravity * Time.fixedDeltaTime;
-            ropeSegments[i] = currentSegment;
+            RopeSegment firstSegment = ropeSegments[i];
+            Vector3 velocity = firstSegment.nowPos - firstSegment.oldPos;
+            firstSegment.oldPos = firstSegment.nowPos;
+            firstSegment.nowPos += velocity;
+            firstSegment.nowPos += gravity * Time.fixedDeltaTime;
+            ropeSegments[i] = firstSegment;
         }
 
         for(int i = 0; i < numberOfSegments; i++)
         {
             this.Constrain();
         }
+
     }
 
     void Update()
     {
-        for (int i = 0; i < numberOfSegments; i++)
-        {
-            RopeSegment ropeSegment = ropeSegments[i];
-            ropeSegment.ropeObject.transform.position = ropeSegments[i].nowPos;
-        }
+      float lineWidth = this.lineWidth;
+      lineRenderer.startWidth = lineWidth;
+      lineRenderer.endWidth = lineWidth;
+
+      Vector3[] ropePositions = new Vector3[totalRopeLength];
+      for (int i = 0; i < totalRopeLength; i++)
+      {
+          ropePositions[i] = this.ropeSegments[i].nowPos;
+      }
+
+      lineRenderer.positionCount = ropePositions.Length;
+      lineRenderer.SetPositions(ropePositions);
     }
 
     void Constrain()
@@ -81,9 +92,9 @@ public class RopeRenderer : MonoBehaviour
 
         RopeSegment firstSegment = this.ropeSegments[0];
         firstSegment.nowPos = ropeStart.transform.position;
-        Vector3 extents = firstSegment.ropeObject.GetComponent<SpriteRenderer>().bounds.extents;
-        firstSegment.nowPos.x += extents.x;
-        firstSegment.nowPos.y -= extents.y;
+        // Vector3 extents = firstSegment.ropeObject.GetComponent<SpriteRenderer>().bounds.extents;
+        // firstSegment.nowPos.x += extents.x;
+        // firstSegment.nowPos.y -= extents.y;
         this.ropeSegments[0] = firstSegment;
 
         RopeSegment lastSegment = this.ropeSegments[numberOfSegments - 1];
@@ -96,15 +107,15 @@ public class RopeRenderer : MonoBehaviour
             RopeSegment secondSegment = ropeSegments[i + 1];
 
             float dist = (firstSegment.nowPos - secondSegment.nowPos).magnitude;
-            float error = Mathf.Abs(dist - firstSegment.ropeLength);
+            float error = Mathf.Abs(dist - ropeSegLen);
 
             Vector3 changeDir = Vector3.zero;
 
-            if (dist > firstSegment.ropeLength)
+            if (dist > ropeSegLen)
             {
                 changeDir = (firstSegment.nowPos - secondSegment.nowPos).normalized;
             }
-            else if (dist < firstSegment.ropeLength)
+            else if (dist < ropeSegLen)
             {
                 changeDir = (secondSegment.nowPos - firstSegment.nowPos).normalized;
             }
@@ -119,107 +130,117 @@ public class RopeRenderer : MonoBehaviour
             this.ropeSegments[i + 1] = secondSegment;
         }
     }
-
-    void AttachInitalRopeSegment(GameObject previousSegment)
-    {
-        GameObject ropeSegment = GenerateRopeSegment(1);
-        ropeSegment.GetComponent<SpriteRenderer>().sortingOrder = 1;
-
-        Vector3 extents = ropeSegment.GetComponent<SpriteRenderer>().bounds.extents;
-
-        Vector3 adjustedRopePosition = ropePosition;
-        adjustedRopePosition.x += extents[0];
-        ropeSegment.transform.position = adjustedRopePosition;
-
-        // Vector3 connectedAnchorPosition = previousSegment.transform.localPosition;
-        // connectedAnchorPosition.x += (extents[0] / scaleFactor);
-        //
-        // previousSegment.AddComponent<HingeJoint2D>();
-        // previousSegment.GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
-        // previousSegment.GetComponent<HingeJoint2D>().connectedBody = ropeSegment.GetComponent<Rigidbody2D>();
-        // previousSegment.GetComponent<HingeJoint2D>().anchor = localRopePosition;
-        // previousSegment.GetComponent<HingeJoint2D>().connectedAnchor = localRopePosition;
-
-        currentSegment = ropeSegment;
-    }
-
-    void AttachRopeSegment(GameObject previousSegment, int segmentNum)
-    {
-        GameObject ropeSegment = GenerateRopeSegment(segmentNum);
-
-        Vector3 extents = ropeSegment.GetComponent<SpriteRenderer>().bounds.extents;
-
-        Vector3 adjustedRopePosition = previousSegment.transform.position;
-        adjustedRopePosition.x += (extents[0] * 2);
-        ropeSegment.transform.position = adjustedRopePosition;
-
-        float unscaledExtent = extents[0] / scaleFactor;
-        Vector3 anchorPosition = new Vector3((float)(unscaledExtent - 0.5f), 0, 0);
-        Vector3 connectedAnchorPosition = new Vector3((float)(-(unscaledExtent - 0.5f)), 0, 0);
-
-        previousSegment.AddComponent<HingeJoint2D>();
-        previousSegment.GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
-        previousSegment.GetComponent<HingeJoint2D>().connectedBody = ropeSegment.GetComponent<Rigidbody2D>();
-        previousSegment.GetComponent<HingeJoint2D>().anchor = anchorPosition;
-        previousSegment.GetComponent<HingeJoint2D>().connectedAnchor = connectedAnchorPosition;
-
-        currentSegment = ropeSegment;
-    }
-
-    void AttachFinalRopeSegment(GameObject previousSegment)
-    {
-        Vector3 finalRopePosition = ropeEnd.transform.localPosition;
-
-        Vector3 extents = previousSegment.GetComponent<SpriteRenderer>().bounds.extents;
-        previousSegment.GetComponent<SpriteRenderer>().sortingOrder = 1;
-        previousSegment.GetComponent<Rigidbody2D>().mass = 0;
-
-        float unscaledExtent = extents[0] / scaleFactor;
-        Vector3 anchorPosition = new Vector3((float)(unscaledExtent - 0.5f), 0, 0);
-
-        // previousSegment.AddComponent<HingeJoint2D>();
-        // previousSegment.GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
-        // previousSegment.GetComponent<HingeJoint2D>().connectedBody = climber2.GetComponent<Rigidbody2D>();
-        // previousSegment.GetComponent<HingeJoint2D>().anchor = anchorPosition;
-        // previousSegment.GetComponent<HingeJoint2D>().connectedAnchor = finalRopePosition;
-    }
-
-    GameObject GenerateRopeSegment(int segmentNum)
-    {
-      GameObject ropeSegment = new GameObject("segment" + segmentNum);
-      ropeSegment.layer = 8;
-      ropeSegment.transform.localScale = new Vector3(scaleFactor, scaleFactor, 1.0f);
-
-      ropeSegment.AddComponent<SpriteRenderer>();
-      ropeSegment.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/rope_segment");
-      ropeSegment.GetComponent<SpriteRenderer>().sortingOrder = 2;
-
-      ropeSegment.AddComponent<Rigidbody2D>();
-      ropeSegment.GetComponent<Rigidbody2D>().mass = mass;
-
-      // ropeSegment.AddComponent<CapsuleCollider2D>();
-      // ropeSegment.GetComponent<CapsuleCollider2D>().direction = CapsuleDirection2D.Horizontal;
-
-      return ropeSegment;
-    }
+    //
+    // void AttachInitalRopeSegment(GameObject previousSegment)
+    // {
+    //     GameObject ropeSegment = GenerateRopeSegment(1);
+    //     ropeSegment.GetComponent<SpriteRenderer>().sortingOrder = 1;
+    //
+    //     Vector3 extents = ropeSegment.GetComponent<SpriteRenderer>().bounds.extents;
+    //
+    //     Vector3 adjustedRopePosition = ropePosition;
+    //     adjustedRopePosition.x += extents[0];
+    //     ropeSegment.transform.position = adjustedRopePosition;
+    //
+    //     // Vector3 connectedAnchorPosition = previousSegment.transform.localPosition;
+    //     // connectedAnchorPosition.x += (extents[0] / scaleFactor);
+    //     //
+    //     // previousSegment.AddComponent<HingeJoint2D>();
+    //     // previousSegment.GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+    //     // previousSegment.GetComponent<HingeJoint2D>().connectedBody = ropeSegment.GetComponent<Rigidbody2D>();
+    //     // previousSegment.GetComponent<HingeJoint2D>().anchor = localRopePosition;
+    //     // previousSegment.GetComponent<HingeJoint2D>().connectedAnchor = localRopePosition;
+    //
+    //     currentSegment = ropeSegment;
+    // }
+    //
+    // void AttachRopeSegment(GameObject previousSegment, int segmentNum)
+    // {
+    //     GameObject ropeSegment = GenerateRopeSegment(segmentNum);
+    //
+    //     Vector3 extents = ropeSegment.GetComponent<SpriteRenderer>().bounds.extents;
+    //
+    //     Vector3 adjustedRopePosition = previousSegment.transform.position;
+    //     adjustedRopePosition.x += (extents[0] * 2);
+    //     ropeSegment.transform.position = adjustedRopePosition;
+    //
+    //     float unscaledExtent = extents[0] / scaleFactor;
+    //     Vector3 anchorPosition = new Vector3((float)(unscaledExtent - 0.5f), 0, 0);
+    //     Vector3 connectedAnchorPosition = new Vector3((float)(-(unscaledExtent - 0.5f)), 0, 0);
+    //
+    //     previousSegment.AddComponent<HingeJoint2D>();
+    //     previousSegment.GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+    //     previousSegment.GetComponent<HingeJoint2D>().connectedBody = ropeSegment.GetComponent<Rigidbody2D>();
+    //     previousSegment.GetComponent<HingeJoint2D>().anchor = anchorPosition;
+    //     previousSegment.GetComponent<HingeJoint2D>().connectedAnchor = connectedAnchorPosition;
+    //
+    //     currentSegment = ropeSegment;
+    //     ropeSegments.Add(new RopeSegment(currentSegment));
+    // }
+    //
+    // void AttachFinalRopeSegment(GameObject previousSegment)
+    // {
+    //     Vector3 finalRopePosition = ropeEnd.transform.localPosition;
+    //
+    //     Vector3 extents = previousSegment.GetComponent<SpriteRenderer>().bounds.extents;
+    //     previousSegment.GetComponent<SpriteRenderer>().sortingOrder = 1;
+    //     previousSegment.GetComponent<Rigidbody2D>().mass = 0;
+    //
+    //     float unscaledExtent = extents[0] / scaleFactor;
+    //     Vector3 anchorPosition = new Vector3((float)(unscaledExtent - 0.5f), 0, 0);
+    //
+    //     // previousSegment.AddComponent<HingeJoint2D>();
+    //     // previousSegment.GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+    //     // previousSegment.GetComponent<HingeJoint2D>().connectedBody = climber2.GetComponent<Rigidbody2D>();
+    //     // previousSegment.GetComponent<HingeJoint2D>().anchor = anchorPosition;
+    //     // previousSegment.GetComponent<HingeJoint2D>().connectedAnchor = finalRopePosition;
+    // }
+    //
+    // void InsertRopeSegment()
+    // {
+    //   AttachRopeSegment()
+    // }
+    //
+    // void RemoveRopeSegment()
+    // {
+    //     GameObject ropeSegment = ropeSegments[numberOfSegments - 2].ropeObject;
+    //     GameObject lastSegment = ropeSegments[numberOfSegments - 1].ropeObject;
+    //     ropeSegments.RemoveAt(numberOfSegments - 1);
+    //     Destroy(lastSegment);
+    //     numberOfSegments -= 1;
+    //
+    //     ropeSegments[numberOfSegments - 1] = new RopeSegment(ropeSegment);
+    //     AttachFinalRopeSegment(ropeSegment);
+    // }
+    //
+    // GameObject GenerateRopeSegment(int segmentNum)
+    // {
+    //   GameObject ropeSegment = new GameObject("segment" + segmentNum);
+    //   ropeSegment.layer = 8;
+    //   ropeSegment.transform.localScale = new Vector3(scaleFactor, scaleFactor, 1.0f);
+    //
+    //   ropeSegment.AddComponent<SpriteRenderer>();
+    //   ropeSegment.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/rope_segment");
+    //   ropeSegment.GetComponent<SpriteRenderer>().sortingOrder = 2;
+    //
+    //   ropeSegment.AddComponent<Rigidbody2D>();
+    //   ropeSegment.GetComponent<Rigidbody2D>().mass = mass;
+    //
+    //   // ropeSegment.AddComponent<CapsuleCollider2D>();
+    //   // ropeSegment.GetComponent<CapsuleCollider2D>().direction = CapsuleDirection2D.Horizontal;
+    //
+    //   return ropeSegment;
+    // }
 
     public struct RopeSegment
     {
-        public GameObject ropeObject;
         public Vector3 nowPos;
         public Vector3 oldPos;
-        public float ropeLength;
 
-        public RopeSegment(GameObject ropeObject)
+        public RopeSegment(Vector3 pos)
         {
-            this.ropeObject = ropeObject;
-
-            Vector3 pos = ropeObject.transform.position;
             this.oldPos = pos;
             this.nowPos = pos;
-
-            Vector3 extents = ropeObject.GetComponent<SpriteRenderer>().bounds.extents;
-            this.ropeLength = extents[0]*2;
         }
     }
 }
