abf8bf9983ac63ff1ba25c34ac51e20cae4c6c6e
Progtart <ramenhero77@hotmail.com>
2021-06-12 17:24:07 -0700

    Created Director and fixed rope physics

---
+++ Assets/Resources/Scripts/Director.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Director : MonoBehaviour
+{
+    public float ropeLength = 20;
+    float ropeSlide = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("length: " + ropeLength);
+    }
+
+    public void shortenRope()
+    {
+        ropeLength += ropeSlide * Time.deltaTime;
+        ropeLength = Mathf.Clamp(ropeLength, 0, 25);
+    }
+
+    public void lengthenRope()
+    {
+        ropeLength -= ropeSlide * Time.deltaTime;
+        ropeLength = Mathf.Clamp(ropeLength, 0, 25);
+    }
+}
